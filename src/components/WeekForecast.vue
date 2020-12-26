5718de3add3b6f3d5bd6243e8bdbdb6a7a7dc323
vikgulb <vikgulb@gmail.com>
2020-12-26 17:44:47 +0200

    get week forecast

--- src/components/WeekForecast.vue
+++ src/components/WeekForecast.vue
@@ -1,75 +1,53 @@
 <template> 
-    <div class='chart' v-if="weekWeather">
-      <div class='chart__column' v-for="day in weekWeather" :key="day.id">
-        <div class='chart__weekDay'>{{ day.day}}</div>
+    <div class='chart' v-if="weekData">
+      <div class='chart__column' v-for="day in weekData" :key="day.dt">
+        <div class='chart__weekDay'>{{ unixToWeekDay(day.dt)}}</div>
         <div class='chart__icon'>
           <icon-base width="25" height="25" viewBox="0 0 42 27" icon-name="cloud"><CloudySvg /></icon-base>
         </div>
         <div class='chart__temp'>
-          <span class='chart__max'>{{ day.max_temp}}</span>
-          <span class='chart__min'>{{ day.min_temp}}</span>
+          <span class='chart__max'>{{ displayTemp(day.temp.max) }}</span>
+          <span class='chart__min'>{{ displayTemp(day.temp.min) }}</span>
         </div>
         <div class='chart__graph'>
           <div class='chart__graph-max'></div>
           <div class='chart__graph-min'></div>
         </div>
       </div>
-  </div>
+    </div>
 </template>
 
 <script>
 import IconBase from '../IconsAsSvg/IconBase.vue'
 import CloudySvg from '../IconsAsSvg/CloudySvg'
+import { API_KEY, URL_BASE } from '../constants'
 
 export default {
   name: 'WeekForecast',
   components: { IconBase, CloudySvg },
   data(){
     return {
-      weekWeather: [
-        {
-          id: 1,
-          day: 'MON',
-          "max_temp": 8,
-          "min_temp": 5,
-        },
-        {
-          id: 2,
-          day: 'TUE',
-          "max_temp": 8,
-          "min_temp": 5,
-        },
-        {
-          id: 3,
-          day: 'WED',
-          "max_temp": 8,
-          "min_temp": 5,
-        },
-        {
-          id: 4,
-          day: 'THU',
-          "max_temp": 6,
-          "min_temp": 2,
-        },
-        {
-          id: 5,
-          day: 'FRI',
-          "max_temp": 8,
-          "min_temp": 5,
-        },
-        {
-          id: 6,
-          day: 'SAT',
-          "max_temp": 8,
-          "min_temp": 5,
-        },
-        {
-          id: 7,
-          day: 'SUN',
-          "max_temp": 8,
-          "min_temp": 5,
-        },
-      ]
+      weekData: []
+    }
+  },
+
+  mounted(){
+    this.getWeekData();
+  },
+
+  methods: {
+    async getWeekData(){
+      const res = await fetch(`${URL_BASE}/onecall?lat=54.687157&lon=25.279652&exclude=alerts,hourly,minutely&appid=${API_KEY}`);
+      const data = await res.json();
+      this.weekData = data.daily;
+      console.log('response', this.weekData)
+    },
+    unixToWeekDay(timeStamp){
+      const dateObject = new Date(timeStamp * 1000)
+      return dateObject.toLocaleString("en-US", {weekday: "short"}).toUpperCase();
+    },
+    displayTemp(temp){
+      return Math.round(temp - 273.15);
     }
   }
 
